8f660376d33e04d7245c95c73bb12e9ad65d7665
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-10 21:33:26 +0700

    Create controllers, routes, views: edit user, updateuser, view user, and delete user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -13,18 +13,67 @@ const userForm = async (req, res) => {
 
 const saveUser = async (req, res) => {
     const {name, email, phone} = req.body;
-    const user = await User.create({
+    await User.create({
         name,
         email,
         phone
     }).catch(error => console.log(error));
-    console.log(user);
 
     await res.redirect('/');
+};
+
+const editUser = async (req, res) => {
+    const {id} = await req.params;
+    const user = await User.findOne({
+        where: {
+            id: id
+        },
+        raw: true
+    }).catch(error => console.log(error))
+    res.render('editUser', {user});
+};
+
+const updateUser = async (req, res) => {
+    const {id} = req.params;
+    const data = req.body;
+    const selector = {
+        where: {id: id}
+    };
+await User.update(data, selector).catch(error => console.log(error))
+
+    res.redirect('/')
+};
+
+const viewUser = async (req, res) => {
+    const {id} = req.params;
+    const user = await User.findOne({
+        where: {
+            id:id
+        },
+        raw: true
+    }).catch(error => console.log(error))
+
+    res.render('viewUser', {user});
+};
+
+const deleteUser = async (req, res) => {
+    const {id} = req.params;
+    await User.destroy({
+        where: {
+            id:id
+        },
+        raw: true
+    }).catch(error => console.log(error))
+
+    res.redirect('/');
 };
 
 module.exports = {
     allUser,
     userForm,
-    saveUser
+    saveUser,
+    editUser,
+    updateUser,
+    viewUser,
+    deleteUser
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { allUser, userForm, saveUser } = require('../controllers/user.js');
+const { allUser, userForm, saveUser, editUser, updateUser, viewUser, deleteUser } = require('../controllers/user.js');
 const router = express.Router();
 
 router.get('/', allUser);
 router.get('/create', userForm);
 router.post('/create', saveUser);
+router.get('/edit/:id', editUser);
+router.post('/update/:id', updateUser);
+router.get('/view/:id', viewUser);
+router.get('/delete/:id', deleteUser);
 
 module.exports = router;
